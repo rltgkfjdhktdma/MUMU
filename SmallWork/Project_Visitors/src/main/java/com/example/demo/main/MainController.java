dd375ac30a8d9620f95b5d59a7abc1f9bb862730
rltgkfjdhktdma <kimc1212@naver.com>
2025-10-17 21:43:17 +0900

    (kwy)
    작업내용 :
    스프링 부트 설치
    간단한 작업을 해보려 했지만 테스트를 위해 로컬로 연결하면 문제 발생
    jsp 파일이 렌더링되지 않고 다운로드 되는 문제 발생
    - 톰캣 버전의 문제, 의존성 등에 의해 발생하는 문제라는 내용들이 있으니 확인하고 처리할것

---
+++ SmallWork/Project_Visitors/src/main/java/com/example/demo/main/MainController.java
@@ -0,0 +1,12 @@
+package com.example.demo.main;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	@GetMapping(value = "/")
+	public String test()  {
+			
+		return "test";
+	}
+}

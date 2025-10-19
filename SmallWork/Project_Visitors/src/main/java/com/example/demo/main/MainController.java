53cb0261fc9fc7ab3cb2f5c1b6052547194c0d08
rltgkfjdhktdma <kimc1212@naver.com>
2025-10-19 23:06:53 +0900

    (kwy)
    jsp 못읽는 문제는 해결
    페이지 작업다음부터 시작. 프론트, 자바(또는 자바스크립트), 백엔드 등 공부

--- SmallWork/Project_Visitors/src/main/java/com/example/demo/main/MainController.java
+++ SmallWork/Project_Visitors/src/main/java/com/example/demo/main/MainController.java
@@ -1,12 +1,13 @@
 package com.example.demo.main;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MainController {
 	@GetMapping(value = "/")
 	public String test()  {
 			
-		return "test";
+		return "Visitor";
 	}
 }
